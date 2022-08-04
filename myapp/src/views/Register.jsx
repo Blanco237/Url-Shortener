366bc2c380fe58fc0944ee62fad238d471568eed
Blanco237 <asongrandy9@gmail.com>
2022-08-04 14:02:44 +0100

    Refactored Register Component

--- myapp/src/views/Register.jsx
+++ myapp/src/views/Register.jsx
@@ -1,75 +1,131 @@
-import React, { useState } from 'react'
+import React, { useState, useContext } from "react";
 
-import classes from '../assets/styles/views/auth.module.css';
+import classes from "../assets/styles/views/auth.module.css";
 
-import GetData from '../components/GetData';
+import logo from "../assets/images/GoogleLogo.svg";
 
-import logo from '../assets/images/GoogleLogo.svg';
-import { createUser,googleSignIn } from '../firebaseUtils';
+import { register } from "../apis/Auth";
 
+import { AlertContext } from "../providers/AlertProvider";
+import { LoadingContext } from "../providers/LoadingProvider";
 
 const Register = () => {
+  const { handleAlert } = useContext(AlertContext);
+  const { handleLoading } = useContext(LoadingContext);
 
-    const [email,setEmail] = useState("");
-    const [password,setPassword] = useState("");
-    const [confirm, setConfirm] = useState("");
-    const [showGetData, setShowGetData] = useState(false);
-    const [user, setUser] = useState(null);
+  const [username, setUsername] = useState("");
+  const [email, setEmail] = useState("");
+  const [phoneNumber, setPhone] = useState(null);
+  const [password, setPassword] = useState("");
+  const [confirm, setConfirm] = useState("");
 
-    function checkValidEntry(){
-        if(email.length<0 || password.length<0){
-            return false;
-        }
-        if(!checkPasswords()){
-            return false;
-        }
-        let regex = new RegExp('[a-z0-9]+@[a-z]+\.[a-z]{2,3}');
-        return regex.test(email);
+  function checkValidEntry() {
+    if (email.length < 0 || password.length < 0) {
+      return false;
     }
-
-    function checkPasswords(){
-        return password==confirm;
+    if (password !== confirm) {
+      return false;
     }
+    let regex = new RegExp("[a-z0-9]+@[a-z]+.[a-z]{2,3}");
+    return regex.test(email);
+  }
 
-    function register(){
-        if(!checkValidEntry()){
-            alert("Please Enter Data Correctly");
-            setEmail("");
-            setPassword("");
-            setConfirm("");
-            return;
-        }
-        createUser(email,password);
+  const handleSubmit = async (type) => {
+    handleLoading(true);
+    if (type === "google") {
+      const res = await register({}, type);
+      console.log(res);
+      if (res.error) {
+        handleLoading(false);
+        handleAlert(res.error);
+      }
+    } else {
+      if (!checkValidEntry()) {
+        handleLoading(false);
+        handleAlert("Please Enter Data Correctly");
+        setEmail("");
+        setPassword("");
+        setConfirm("");
+        return;
+      }
+      const data = {
+        username,
+        email,
+        phoneNumber,
+        password,
+      };
+      const res = await register(data, type);
+      if (res.error) {
+        handleLoading(false);
+        handleAlert(res.error);
+      }
     }
+    handleLoading(false);
+  };
 
-    async function loginWithGoogle(){
-        let size = window.innerWidth;
-        let state = size<600? 'mobile' : 'desktop';
-        let t = await googleSignIn(state);
-        setUser(t);
-        setShowGetData(true);
-    }
-
-    return (
-        <div className={classes.body}>
-            <div className={classes.form__container}>
-                <h3>GooLnk Registration</h3>
-                <div className={classes.form__google} onClick={loginWithGoogle}>
-                    <img src={logo} />
-                    <p>Login with Google</p>
-                </div>
-                <form className={classes.form} autocomplete="off">
-                    <input type="text" name="username" placeholder="Enter Username" />
-                    <input type="text" name="email" placeholder="Enter Your Email..." autoComplete={`false`} />
-                    <input type="password" name="password" placeholder="Enter Your Password..." />
-                    <input type="password"  name="confirm" placeholder="Confirm Password..." />
-                    <button type="submit" onClick={register}>Submit</button>
-                </form>
-                <p className={classes.options}>Already have an Account? <span>Login</span></p>
-            </div>
-            { showGetData && <GetData user={user}/> }
+  return (
+    <div className={classes.body}>
+      <div className={classes.form__container}>
+        <h3>GooLnk Registration</h3>
+        <div
+          className={classes.form__google}
+          onClick={() => handleSubmit("google")}
+        >
+          <img src={logo} />
+          <p>Login with Google</p>
         </div>
-    )
-}
+        <form
+          className={classes.form}
+          autocomplete="off"
+          onSubmit={(e) => e.preventDefault()}
+        >
+          <input
+            type="text"
+            name="username"
+            value={username}
+            onChange={(e) => setUsername(e.target.value)}
+            placeholder="Enter Username"
+          />
+          <input
+            type="email"
+            name="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="Enter Your Email..."
+            autoComplete={`false`}
+          />
+          <input
+            type="number"
+            name="phone"
+            value={phoneNumber}
+            onChange={(e) => setPhone(e.target.value)}
+            placeholder="Enter Your Phone Number..."
+            autoComplete={`false`}
+          />
+          <input
+            type="password"
+            name="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Enter Your Password..."
+          />
+          <input
+            type="password"
+            name="confirm"
+            value={confirm}
+            onChange={(e) => setConfirm(e.target.value)}
+            placeholder="Confirm Password..."
+          />
+          <button type="submit" onClick={handleSubmit}>
+            Submit
+          </button>
+        </form>
+        <p className={classes.options}>
+          Already have an Account? <span>Login</span>
+        </p>
+      </div>
+    </div>
+  );
+};
 
-export default Register
+export default Register;
